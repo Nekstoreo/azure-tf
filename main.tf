b37b3453a399c0510a04d06798b19af1b4745094
Nekstoreo450 <97989097+Nekstoreo450@users.noreply.github.com>
2022-10-12 10:21:58 -0500

    Configuracion terraform

---
+++ main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "my1ResourceGroup"
+  location = var.location
+}
+# Virtual Machine Resources
+
+resource "azurerm_virtual_network" "rg" {
+  name                = "rg-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "rg" {
+  name                 = "rg-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.rg.name
+  address_prefixes     = ["10.0.4.0/24"]
+}
+
+resource "azurerm_network_interface" "rg" {
+  name                = "rg-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "rg-ip"
+    subnet_id                     = azurerm_subnet.rg.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_windows_virtual_machine" "rg" {
+  name                = "myTF-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "adminuser"
+  admin_password      = "P@ssw0rd1234!"
+
+  network_interface_ids = [
+    azurerm_network_interface.rg.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-datacenter-gensecond"
+    version   = "latest"
+  }
+
+  patch_mode = "AutomaticByPlatform"
+}
