467a753314e4d9b47d73daf537395e11132ad0c9
Nekstoreo450 <97989097+Nekstoreo450@users.noreply.github.com>
2022-10-12 20:20:57 -0500

    Update main.tf assign ip

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
+##########################################################################################
 terraform {
   required_version = ">= 1.1.0"
 }
-
+##########################################################################################
 provider "azurerm" {
   features {}
 }
-
+##########################################################################################
 #Creacion del grupo de recursos
 resource "azurerm_resource_group" "rg" {
   #Puede cambiar el nombre del grupo de recursos
@@ -13,23 +14,46 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 # Virtual Machine Resources
-  ## Red de la maquina
+##########################################################################################
+## Red de la maquina
 resource "azurerm_virtual_network" "rg" {
   name                = "rg-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
-
-  ## Subred de la maquina
+##########################################################################################
+## Subred de la maquina
 resource "azurerm_subnet" "rg" {
   name                 = "rg-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.rg.name
   address_prefixes     = ["10.0.4.0/24"]
 }
+##########################################################################################
+## IP publica
+resource "azurerm_public_ip" "rg" {
+  name                = "myTerraF-PublicIp1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+  sku_tier            = "Regional"
 
-  ## Interfaz de red
+}
+
+data "azurerm_public_ip" "rg" {
+  name                = azurerm_public_ip.rg.name
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+output "public_ip_address" {
+  value = data.azurerm_public_ip.rg.ip_address
+}
+
+##########################################################################################
+
+## Interfaz de red
 resource "azurerm_network_interface" "rg" {
   name                = "rg-nic"
   location            = azurerm_resource_group.rg.location
@@ -39,30 +63,21 @@ resource "azurerm_network_interface" "rg" {
     name                          = "rg-ip"
     subnet_id                     = azurerm_subnet.rg.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.rg.id
   }
 }
-
-  ## IP publica de la maquina
-resource "azurerm_public_ip" "rg" {
-  name                = "acceptanceTestPublicIp1"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku = "Basic"
-  sku_tier = "Regional"
-  allocation_method   = "Static"
-}
-
-  # Tamaño y demas caracteristicas de la maquina
+##########################################################################################
+# Tamaño y demas caracteristicas de la maquina
 resource "azurerm_windows_virtual_machine" "rg" {
   name                = "myTerraF-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   ##Tamaño
-  size                = "Standard_DS1_v2"
+  size = "Standard_DS1_v2"
   ##Nombre de administador
-  admin_username      = "adminuser"
+  admin_username = "adminuser"
   ##Contraseña de administador
-  admin_password      = "P@ssw0rd1234!"
+  admin_password = "P@ssw0rd1234!"
 
   network_interface_ids = [
     azurerm_network_interface.rg.id,
@@ -82,3 +97,4 @@ resource "azurerm_windows_virtual_machine" "rg" {
 
   patch_mode = "AutomaticByPlatform"
 }
+##########################################################################################
