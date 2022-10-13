606af88434b4382a9e8fcf03c0c5ab648ccb092d
Nekstoreo450 <97989097+Nekstoreo450@users.noreply.github.com>
2022-10-12 20:29:45 -0500

    Update

--- main.tf
+++ main.tf
@@ -33,12 +33,12 @@ resource "azurerm_subnet" "rg" {
 ##########################################################################################
 ## IP publica
 resource "azurerm_public_ip" "rg" {
-  name                = "myTerraF-PublicIp1"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Basic"
-  sku_tier            = "Regional"
+  name                         = "myTerraF-PublicIp1"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  public_ip_address_allocation = "Static"
+  sku                          = "Basic"
+  sku_tier                     = "Regional"
 
 }
 
